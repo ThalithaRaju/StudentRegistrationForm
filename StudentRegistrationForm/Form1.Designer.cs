db07b0afb1e70610ecfcc4d84be15b2e081822b8
ThalithaRaju <thalitharaju22@gmail.com>
2020-10-07 15:13:35 +0530

    form registrarion database

---
+++ StudentRegistrationForm/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace StudentRegistrationForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Registrartion = new System.Windows.Forms.Label();
+            this.lblMinimize = new System.Windows.Forms.Label();
+            this.lblClose = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblSave = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbUniversity = new System.Windows.Forms.ComboBox();
+            this.cmbCollege = new System.Windows.Forms.ComboBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(29, 29);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Registrartion
+            // 
+            this.Registrartion.AutoSize = true;
+            this.Registrartion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Registrartion.Location = new System.Drawing.Point(36, 10);
+            this.Registrartion.Name = "Registrartion";
+            this.Registrartion.Size = new System.Drawing.Size(80, 16);
+            this.Registrartion.TabIndex = 1;
+            this.Registrartion.Text = "Registration";
+            // 
+            // lblMinimize
+            // 
+            this.lblMinimize.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinimize.Location = new System.Drawing.Point(254, 9);
+            this.lblMinimize.Name = "lblMinimize";
+            this.lblMinimize.Size = new System.Drawing.Size(12, 22);
+            this.lblMinimize.TabIndex = 2;
+            this.lblMinimize.Text = "-";
+            this.lblMinimize.Click += new System.EventHandler(this.lblMinimize_Click);
+            // 
+            // lblClose
+            // 
+            this.lblClose.AutoSize = true;
+            this.lblClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClose.Location = new System.Drawing.Point(288, 13);
+            this.lblClose.Name = "lblClose";
+            this.lblClose.Size = new System.Drawing.Size(16, 16);
+            this.lblClose.TabIndex = 4;
+            this.lblClose.Text = "X";
+            this.lblClose.Click += new System.EventHandler(this.lblClose_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblSave);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.cmbUniversity);
+            this.panel1.Controls.Add(this.cmbCollege);
+            this.panel1.Controls.Add(this.txtPhone);
+            this.panel1.Controls.Add(this.txtEmail);
+            this.panel1.Controls.Add(this.txtName);
+            this.panel1.Location = new System.Drawing.Point(1, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(303, 324);
+            this.panel1.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // lblSave
+            // 
+            this.lblSave.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSave.ForeColor = System.Drawing.Color.White;
+            this.lblSave.Location = new System.Drawing.Point(103, 280);
+            this.lblSave.Name = "lblSave";
+            this.lblSave.Size = new System.Drawing.Size(100, 23);
+            this.lblSave.TabIndex = 12;
+            this.lblSave.Text = "SAVE";
+            this.lblSave.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblSave.Click += new System.EventHandler(this.lblSave_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(41, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(35, 224);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "College";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(163, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "University";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "PhoneNumber";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Email";
+            // 
+            // cmbUniversity
+            // 
+            this.cmbUniversity.FormattingEnabled = true;
+            this.cmbUniversity.Location = new System.Drawing.Point(166, 240);
+            this.cmbUniversity.Name = "cmbUniversity";
+            this.cmbUniversity.Size = new System.Drawing.Size(99, 21);
+            this.cmbUniversity.TabIndex = 4;
+            this.cmbUniversity.SelectedIndexChanged += new System.EventHandler(this.cmbUniversity_SelectedIndexChanged);
+            // 
+            // cmbCollege
+            // 
+            this.cmbCollege.FormattingEnabled = true;
+            this.cmbCollege.Location = new System.Drawing.Point(38, 240);
+            this.cmbCollege.Name = "cmbCollege";
+            this.cmbCollege.Size = new System.Drawing.Size(112, 21);
+            this.cmbCollege.TabIndex = 3;
+            this.cmbCollege.SelectedIndexChanged += new System.EventHandler(this.cmbCollege_SelectedIndexChanged);
+            this.cmbCollege.SelectedValueChanged += new System.EventHandler(this.cmbCollege_SelectedValueChanged);
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(38, 174);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(236, 20);
+            this.txtPhone.TabIndex = 2;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(38, 111);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(236, 20);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(38, 47);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(236, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(304, 365);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblClose);
+            this.Controls.Add(this.lblMinimize);
+            this.Controls.Add(this.Registrartion);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Registrartion;
+        private System.Windows.Forms.Label lblMinimize;
+        private System.Windows.Forms.Label lblClose;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbUniversity;
+        private System.Windows.Forms.ComboBox cmbCollege;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblSave;
+    }
+}
+
---
+++ StudentRegistrationForm/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentRegistrationForm
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<int, string> numberNames = new Dictionary<int, string>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblClose_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numberNames.Add(1, "MCA");
+           // cmbCollege.DataSource = new BindingSource(numberNames, null);
+            cmbCollege.DisplayMember ="MCA";
+            cmbCollege.ValueMember = "key";
+            //string value = ((KeyValuePair<int, string>)cmbCollege.SelectedValue).Value.ToString();
+
+            #region Design
+            //lblSave.BackColor = Color.FromArgb(41,141,235);
+            label1.ForeColor = Color.FromArgb(41, 141, 235);
+            label2.ForeColor = Color.FromArgb(41, 141, 235);
+            label3.ForeColor = Color.FromArgb(41, 141, 235);
+            label4.ForeColor = Color.FromArgb(41, 141, 235);
+            label5.ForeColor = Color.FromArgb(41, 141, 235);
+            lblSave.BackColor = Color.FromArgb(41, 141, 235);
+            #endregion
+            
+            SqlConnection con = new SqlConnection(@"Server=LAPTOP-Q2N7MAED\SQLEXPRESS ;Database=CollegeCMS;Trusted_Connection=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("UniversityGet", con);
+            // MessageBox.Show("connected");
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+             cmbUniversity.Items.Add(dr["Name"].ToString());
+            }
+            con.Close();
+          
+        }
+
+        private void lblMinimize_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void lblSave_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Server=LAPTOP-Q2N7MAED\SQLEXPRESS ;Database=CollegeCMS;Trusted_Connection=True");
+            conn.Open();
+            SqlCommand cmdd = new SqlCommand("StudentSave", conn);
+            cmdd.CommandType = CommandType.StoredProcedure;
+            cmdd.Parameters.AddWithValue("@Name", txtName.Text);
+            cmdd.Parameters.AddWithValue("@Email", txtEmail.Text);
+            cmdd.Parameters.AddWithValue("@Name", txtPhone.Text);
+          //  cmdd.Parameters.AddWithValue("@CollegeId",);
+            conn.Close();
+        }
+
+        private void cmbUniversity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbCollege.Items.Clear();
+            SqlConnection conn = new SqlConnection(@"Server=LAPTOP-Q2N7MAED\SQLEXPRESS ;Database=CollegeCMS;Trusted_Connection=True");
+            conn.Open();
+            SqlCommand cmdd = new SqlCommand("CollegeGet", conn);
+            cmdd.CommandType = CommandType.StoredProcedure;
+            cmdd.Parameters.AddWithValue("@Name",cmbUniversity.SelectedItem);
+            SqlDataReader drr = cmdd.ExecuteReader();
+            while (drr.Read())
+            {
+                cmbCollege.Items.Add(drr["Name"].ToString());
+            }
+            conn.Close();
+            
+        }
+
+        private void cmbCollege_SelectedValueChanged(object sender, EventArgs e)
+        {
+           // string value = ((KeyValuePair<int, string>)cmbCollege.SelectedValue).Value.ToString();
+        }
+
+        private void cmbCollege_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbCollege.SelectedItem.ToString();
+            if (numberNames.ContainsValue(cmbCollege.SelectedItem.ToString()))
+            {
+                MessageBox.Show("Test");
+            }
+               
+        }
+    }
+}
